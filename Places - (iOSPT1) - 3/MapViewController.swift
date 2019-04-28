71257bf641e1389f759984dcf1ea727ee417f8d5
Dongwoo Pae <dongwoopae@gmail.com>
2019-04-28 16:18:51 -0700

    setting up views

---
+++ Places - (iOSPT1) - 3/MapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewController.swift
+//  Places - (iOSPT1) - 3
+//
+//  Created by Dongwoo Pae on 4/28/19.
+//  Copyright © 2019 Dongwoo Pae. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
